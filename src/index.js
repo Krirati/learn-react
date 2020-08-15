5b62886b7128ad5b6b6e940d37956a131d773622
krirati <kriratiODDS@odds.team>
2020-08-15 11:03:42 +0700

    component function props

--- src/index.js
+++ src/index.js
@@ -4,9 +4,37 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+function WithoutJSX() {
+  return React.createElement('h1', {alt:'test'}, 'Without JSX');
+}
+
+//ถ้าจะเขียนเกิo  1  บรรทัดจะต้องใส่อะไรครอบไว้ก่อน
+//expression
+
+//function compoenet
+function HelloWold(props) { //({title}) จะสั้นชึ้น เอา title ออกมาเลย
+  const {title} = props;
+  return <div>
+      <h1 alt={'test'}>With JSX {props.title}</h1>
+    </div>
+}
+
+
+class HelloWoldComp extends React.Component {
+   render() {
+  //  return <h1>Hellow World class Component {this.props.title}</h1> // ตั้งแต่มี react.component จะมี props
+    return <div>
+      <HelloWold title="compoent int component"/>
+      <HelloWold title="component in component"/>
+    </div>
+   }
+ }
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <HelloWold title="Hello function"/>
+    <HelloWoldComp title={`hello component`}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
